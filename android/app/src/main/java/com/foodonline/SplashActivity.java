13050bb8ccce4763655278284267901e8d5b2bb7
Yogeshwari <raoyogeshwari81@gmail.com>
2022-11-17 23:07:42 +0530

    Splash Screen changes

--- android/app/src/main/java/com/foodonline/SplashActivity.java
+++ android/app/src/main/java/com/foodonline/SplashActivity.java
@@ -1,25 +1,16 @@
 package com.foodonline;
 
-import android.app.Application;
-import android.content.Context;
-import com.facebook.react.PackageList;
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.config.ReactFeatureFlags;
-import com.facebook.soloader.SoLoader;
-import com.foodonline.newarchitecture.MainApplicationReactNativeHost;
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
-
-public class SplashActivity extends AppCompatActivity{
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+ 
+public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this,MainAcitivity.class);
+
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
-  
 }
