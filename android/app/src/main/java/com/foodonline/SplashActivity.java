7be5ab0a8ec879cbf81785fcbe735beb2da1babe
Yogeshwari <raoyogeshwari81@gmail.com>
2022-11-20 14:46:15 +0530

    Revert "Splash Screen changes"
    This reverts commit 13050bb8ccce4763655278284267901e8d5b2bb7.

--- android/app/src/main/java/com/foodonline/SplashActivity.java
+++ android/app/src/main/java/com/foodonline/SplashActivity.java
@@ -1,16 +1,25 @@
 package com.foodonline;
 
-import android.content.Intent;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
- 
-public class SplashActivity extends AppCompatActivity {
+import android.app.Application;
+import android.content.Context;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
+import com.facebook.soloader.SoLoader;
+import com.foodonline.newarchitecture.MainApplicationReactNativeHost;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+public class SplashActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this,MainAcitivity.class);
         startActivity(intent);
         finish();
     }
+  
 }
